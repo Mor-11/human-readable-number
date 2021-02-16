39da698df83add2a76383d694ba7d6feeed1209d
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-16 18:27:53 +0300

    fix: change terms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable(number) {
-    let arr = String(number).split(""); // число переводим в массив
+    
     // массив первых девятнадцати чисел
     let FN = [
         "zero",
@@ -36,13 +36,14 @@ module.exports = function toReadable(number) {
         "eighty",
         "ninety",
     ];
+    let arr = String(number).split(""); // число переводим в массив
 
-    if (number > 100) {
+    if (number < 100) {
         if (number < 20) return FN[number]; //числа от 0 до 20
         if (number < 100 && number % 10 == 0) return D[arr[0]]; // десятки 10, 20, 30 ...
         if (number < 100) return D[arr[0]] + " " + FN[arr[1]]; //  числа от 21 до 99
     }
-    if (number < 100) {
+    if (number >= 100) {
         if (number % 100 == 0) return FN[arr[0]] + " hundred"; // сотни 100, 200 ...
         if (number > 99 && number % 100 < 20)  //  сотни, где десятки от 1 до 19
             return FN[arr[0]] + " hundred " + FN[number % 100];
@@ -51,3 +52,4 @@ module.exports = function toReadable(number) {
         return FN[arr[0]] + " hundred " + D[arr[1]] + " " + FN[arr[2]]; // все остальные числа 
     }
 };
+
